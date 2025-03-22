7ca9a5ad37726052d7eda04b916f4bcf0bcfe4f6
himchan94 <himchan9494@gmail.com>
2025-03-22 14:51:09 +0900

    feat: add deploy yml

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import createMDX from "@next/mdx";
 import rehypeHighlight from "rehype-highlight";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const basePath = isGithubActions ? "/devhimchan" : "";
+
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   images: {
@@ -11,6 +14,10 @@ const nextConfig = {
       },
     ],
   },
+  basePath,
+  assetPrefix: basePath,
+  output: "export" as const,
+  distDir: "out",
 };
 
 const withMDX = createMDX({
